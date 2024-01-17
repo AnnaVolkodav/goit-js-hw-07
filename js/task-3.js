84cdbf3c7a16236eb6c8792085ba2f9fb7cd8db2
AnnaVolkodav <annvolkodav@gmail.com>
2024-01-17 23:55:32 +0200

    task-3 and task-5 => over
    Only task-6

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,6 @@ const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
 
 nameInput.addEventListener("input", function(event) {
-  const trimmedName = event.target.value.trim(); // видаляємо пробіли з обох країв
+  const trimmedName = event.target.value.trim(); // видалення пробілів з обох країв
   nameOutput.textContent = trimmedName !== "" ? trimmedName : "Anonymous";
 });
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorName = document.querySelector('span.color');
+const colorChangeBtn = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+
+
+colorChangeBtn.addEventListener('click', function() {
+  const randomChangeColor = getRandomHexColor();
+  body.style.backgroundColor = randomChangeColor;
+  colorName.textContent = randomChangeColor;
+});
